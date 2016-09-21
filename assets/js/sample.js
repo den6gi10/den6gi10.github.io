fa6b6b858831bd0211048bcb42f7a8ed5219de4b
user <user@user-no-MacBook-Air.local>
2016-09-22 02:52:17 +0900

    28th  commit test Modifed

---
+++ assets/js/sample.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var feed = new Instafeed({
+        get: 'tagged', //ハッシュタグから取得
+        tagName: '味とめ食事部', //好きなハッシュタグを設定
+        sortBy:'random', //並び順をランダムに
+        links: true , //画像リンク取得
+        limit: 3, //取得する画像数を設定
+        resolution: 'low_resolution', //画像サイズを設定
+        template: '<li><a href="{{link}}"><img src="{{image}}" target="_blank" /></a><span>{{caption}}</span><span>like:{{likes}},comments:{{comments}}</span></li>',
+        clientId: 'b24b410a8d724942b564581d06238378' //Instagram DevelopersのクライアントID
+    });
+    feed.run();
+});
